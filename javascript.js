7b6fbb71cde28e7059c5f945e0f68e1949e75b75
Srijan <srijanghosh28@gmail.com>
2025-04-28 13:09:20 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let count = 1;
+
+function add() {
+    const now = new Date(); // Get current date and time
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const input = document.querySelector("#inputbox");
+    let p = document.createElement("p");
+    p.innerText = count + ". " + input.value + " | " + hours + ":" + minutes;
+    document.querySelector(".task-list").appendChild(p)
+    count++;
+
+}
